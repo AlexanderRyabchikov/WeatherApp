4bbc54481e811e52e6ade5becdcebde179313da8
Alexander Ryabchikov <alexasp3@gmail.com>
2018-04-16 16:07:18 +0300

    Добавлена верстка action bar в приложении

--- app/src/main/java/com/example/skender/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/skender/weatherapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.skender.weatherapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.title_action_bar_main);
 
         Weather weather;
 
